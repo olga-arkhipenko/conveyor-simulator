62c76393f8b347d5a97a84003c0a3fce880e4916
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2020-01-11 18:45:27 +0200

    chore: linter config (#6)
    * add eslint, husky, prettier and pretty-quick
    
    * add eslint config, eslint prettier plugin
    
    * fix linting errors :)
    
    * add lint script

--- index.js
+++ index.js
@@ -5,62 +5,61 @@
 
 // T = t * (m + n - 1)
 
-const sleep = async ms => new Promise(resolve => setTimeout(resolve, ms)); //helpers
+const sleep = async ms => new Promise(resolve => setTimeout(resolve, ms)); // helpers
 
-const storage = document.querySelector('.items');
-const conveyor = document.querySelector('.stages');
+const STORAGE = document.querySelector(".items");
+const CONVEYOR = document.querySelector(".stages");
 
 const loadStorage = (itemsAmount, storage) => {
-  for (let i = 1; i <= itemsAmount; i++) {
-    const itemNode = document.createElement('div');
+  for (let i = 1; i <= itemsAmount; i += 1) {
+    const itemNode = document.createElement("div");
     itemNode.id = `item ${i}`;
-    itemNode.className = 'item';
+    itemNode.className = "item";
     itemNode.innerText = `Item ${i}`;
     storage.appendChild(itemNode);
   }
 };
 
 // n = 6
-loadStorage(6, storage);
+loadStorage(6, STORAGE);
 
 const createStageNode = (stageId, stageLabel) => {
-  const stageNode = document.createElement('div');
-  stageNode.className = 'stage';
-  const stageInnerNode = document.createElement('div');
+  const stageNode = document.createElement("div");
+  stageNode.className = "stage";
+  const stageInnerNode = document.createElement("div");
   stageInnerNode.id = stageId;
-  stageInnerNode.className = 'stage__inner';
-  const stageLabelNode = document.createElement('p');
+  stageInnerNode.className = "stage__inner";
+  const stageLabelNode = document.createElement("p");
   stageLabelNode.innerText = stageLabel;
-  stageLabelNode.className = 'stage__label';
+  stageLabelNode.className = "stage__label";
   stageNode.appendChild(stageInnerNode);
   stageNode.appendChild(stageLabelNode);
   return stageNode;
 };
 
 const loadConveyor = (stagesAmount, conveyor) => {
-  for (let i = 1; i <= stagesAmount; i++) {
+  for (let i = 1; i <= stagesAmount; i += 1) {
     const stageNode = createStageNode(`stage ${i}`, `Stage ${i}`);
     conveyor.appendChild(stageNode);
   }
 };
 
 // m = 5
-loadConveyor(5, conveyor);
+loadConveyor(5, CONVEYOR);
 
 // t = 3s
 const manufacture = async item => {
-  const stages = document.querySelectorAll('.stage__inner')
-  stages.forEach(stage => {
+  const stages = document.querySelectorAll(".stage__inner");
+  stages.forEach(async stage => {
     stage.appendChild(item);
     await sleep(3000);
     stage.removeChild(item);
   });
-}
-
+};
 
 const startConveyor = () => {
-  const items = document.querySelectorAll('.item');
+  const items = document.querySelectorAll(".item");
   items.forEach(item => manufacture(item));
 };
 
-
+startConveyor();
