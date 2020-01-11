1b9e0c70fe49fb7105e02f579e29c29004233fa5
olga-arkhipenko <26512909+olga-arkhipenko@users.noreply.github.com>
2020-01-11 19:38:27 +0300

    chore: formatter config (#5)
    * add csscomb and editor configs
    
    * add .vscode folder to gitignore
    
    * format files

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ const storage = document.querySelector('.items');
 const conveyor = document.querySelector('.stages');
 
 const loadStorage = (itemsAmount, storage) => {
-    for(let i = 1; i <= itemsAmount; i++) {
-        const itemNode = document.createElement('div');
-        itemNode.id = `item ${i}`;
-        itemNode.className = 'item';
-        itemNode.innerText = `Item ${i}`;
-        storage.appendChild(itemNode);
-    }
+  for (let i = 1; i <= itemsAmount; i++) {
+    const itemNode = document.createElement('div');
+    itemNode.id = `item ${i}`;
+    itemNode.className = 'item';
+    itemNode.innerText = `Item ${i}`;
+    storage.appendChild(itemNode);
+  }
 };
 
 // n = 6
 loadStorage(6, storage);
 
 const createStageNode = (stageId, stageLabel) => {
-    const stageNode = document.createElement('div');
-    stageNode.className = 'stage';
-    const stageInnerNode = document.createElement('div');
-    stageInnerNode.id = stageId;
-    stageInnerNode.className = 'stage__inner';
-    const stageLabelNode = document.createElement('p');
-    stageLabelNode.innerText = stageLabel;
-    stageLabelNode.className = 'stage__label';
-    stageNode.appendChild(stageInnerNode);
-    stageNode.appendChild(stageLabelNode);
-    return stageNode;
+  const stageNode = document.createElement('div');
+  stageNode.className = 'stage';
+  const stageInnerNode = document.createElement('div');
+  stageInnerNode.id = stageId;
+  stageInnerNode.className = 'stage__inner';
+  const stageLabelNode = document.createElement('p');
+  stageLabelNode.innerText = stageLabel;
+  stageLabelNode.className = 'stage__label';
+  stageNode.appendChild(stageInnerNode);
+  stageNode.appendChild(stageLabelNode);
+  return stageNode;
 };
 
 const loadConveyor = (stagesAmount, conveyor) => {
-    for(let i = 1; i <= stagesAmount; i++) {
-        const stageNode = createStageNode(`stage ${i}`, `Stage ${i}`);
-        conveyor.appendChild(stageNode);
-    }
+  for (let i = 1; i <= stagesAmount; i++) {
+    const stageNode = createStageNode(`stage ${i}`, `Stage ${i}`);
+    conveyor.appendChild(stageNode);
+  }
 };
 
 // m = 5
@@ -49,18 +49,18 @@ loadConveyor(5, conveyor);
 
 // t = 3s
 const manufacture = async item => {
-    const stages = document.querySelectorAll('.stage__inner')
-    stages.forEach(stage => {
-        stage.appendChild(item);
-        await sleep(3000);
-        stage.removeChild(item);
-    });
+  const stages = document.querySelectorAll('.stage__inner')
+  stages.forEach(stage => {
+    stage.appendChild(item);
+    await sleep(3000);
+    stage.removeChild(item);
+  });
 }
 
 
 const startConveyor = () => {
-    const items = document.querySelectorAll('.item');
-    items.forEach(item => manufacture(item));
+  const items = document.querySelectorAll('.item');
+  items.forEach(item => manufacture(item));
 };
 
 
