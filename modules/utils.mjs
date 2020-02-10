f5244944f6cbfc6a5fbfd477215047091e06717e
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2020-02-10 05:21:06 +0300

    feat: utility function for index range generation

--- modules/utils.mjs
+++ modules/utils.mjs
@@ -1,2 +1,8 @@
 export const sleep = async ms =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * Generate an array of indices up to the given `count`.
+ * @param {number} count - upper limit of the index range.
+ */
+export const indexRange = count => Array(count).map((_, idx) => idx);
